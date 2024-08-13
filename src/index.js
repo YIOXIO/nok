8894287be4db9ff758ba5b816f3f8398745a5e36
unknown <C:\The Bat!>
2024-08-13 11:10:10 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './index.css'
+
+
+document.querySelector('.burger-menu').addEventListener('click', function () {
+    document.querySelector('.side-navbar').classList.toggle('side-navbar-open');
+  });
+  
+  const burgerMenu = document.querySelector('.burger-menu');
+  
+  burgerMenu.addEventListener('click', function () {
+    this.classList.toggle('active');
+  });
